293aee4cb7e5ff1fc3b6fa3fb62079c7d5497598
Hendrik Makait <hendrik.makait@gmail.com>
2019-05-13 10:29:22 +0200

    Fix load scripts

--- scripts/luftdaten/load_csv.sh
+++ scripts/luftdaten/load_csv.sh
@@ -2,6 +2,6 @@
 d=$1
 while [ "$d" != "$2" ]; do
   echo $d
-  wget --directory-prefix=data/raw --recursive --no-parent --no-host-directories --accept csv archive.luftdaten.info/"$d"
+  wget --directory-prefix=data/raw --recursive --no-parent --no-host-directories --accept csv https://archive.luftdaten.info/"$d"/
   d=$(date -I -d "$d + 1 day")
 done
--- scripts/luftdaten/load_parquet.sh
+++ scripts/luftdaten/load_parquet.sh
@@ -2,6 +2,6 @@
 d=$1
 while [ "$d" != "$2" ]; do
   month=$(date -ud "$d" +%Y-%m)
-  wget --directory-prefix=data/raw --recursive --no-parent --no-host-directories --accept parquet archive.luftdaten.info/parquet/"$month"
+  wget --directory-prefix=data/raw --recursive --no-parent --no-host-directories --accept parquet https://archive.luftdaten.info/parquet/"$month"/
   d=$(date -I -d "$d + 1 months")
 done
