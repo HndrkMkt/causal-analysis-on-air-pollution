e4c51785d3f80a85dcad0b6f84a39764a8519146
Hendrik Makait <hendrik.makait@gmail.com>
2019-06-16 12:45:35 +0200

    Adjust causal analysis pipeline

--- notebooks/causal_discovery.py
+++ notebooks/causal_discovery.py
@@ -4,47 +4,77 @@ from tigramite import plotting as tp
 from tigramite.pcmci import PCMCI
 from tigramite.independence_tests import ParCorr, GPDC, CMIknn, CMIsymb
 
-var_names = ["dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "temperature", "humidity", "p1", "p2"]
+def load_data():
+    sensor_data = pd.read_csv("../data/processed/causalDiscoveryData.csv", sep=";", names=[
+        "location", "lat", "lon", "timestamp", "dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "pressure", "altitude", "pressure_sealevel", "temperature",
+        "humidity_sensor", "p1", "p2", "p0", "durP1", "ratioP1", "durP2", "ratioP2",
+        "apparent_temperature", "cloud_cover", "dew_point", "humidity_weather", "ozone", "precip_intensity", "precip_probability",
+        "precip_type", "pressure", "uv_index", "visibility", "wind_bearing", "wind_gust", "wind_speed"], true_values=["true"], false_values=["false"])
 
-
-def create_dataframe(var_names):
-    sensor_data = pd.read_csv("../data/processed/output.csv", sep=";", names=[
-        "location", "lat", "lon", "timestamp", "dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "pressure",
-        "altitude", "pressure_sealevel", "temperature",
-        "humidity", "p1", "p2", "p0", "durP1", "ratioP1", "durP2", "ratioP2"], true_values=["true"],
-                              false_values=["false"])
-
-    sensor_data["timestamp"] = pd.to_datetime(sensor_data["timestamp"])
-    sensor_data["isWeekend"] = sensor_data["isWeekend"].astype(int)
+    sensor_data.loc[:, "timestamp"] = pd.to_datetime(sensor_data["timestamp"])
+    sensor_data.loc[:, "isWeekend"] = sensor_data["isWeekend"].astype(int)
 
     sensor_data = sensor_data.sort_values(by=["location", "timestamp"])
-    filtered_sensor_data = sensor_data[
-        ["dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "temperature", "humidity", "p1", "p2"] + ["location",
-                                                                                                         "timestamp"]]
-    filtered_sensor_data = filtered_sensor_data.dropna()
+    return sensor_data
 
-    localized_data = filtered_sensor_data[filtered_sensor_data["location"] == 716]
-    localized_data = localized_data.head(2000)
-    data = localized_data[var_names]
-    datatime = localized_data["timestamp"]
+def extract_dataset(sensor_data):
+    sensor_data = sensor_data.loc[:, ["location", "lat", "lon", "timestamp", "dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "temperature",
+                               "humidity_sensor", "p1", "p2", "apparent_temperature", "cloud_cover", "dew_point", "humidity_weather", "precip_intensity", "precip_probability",
+                               "visibility", "wind_bearing", "wind_gust", "wind_speed"]]
+    sensor716 = sensor_data.loc[sensor_data["location"] == 716]
+    sensor716.loc[sensor716["wind_gust"].isna(), "wind_gust"] = sensor716[sensor716["wind_gust"].isna()]["wind_speed"]
+    sensor716.loc[sensor716["wind_bearing"].isna(), "wind_bearing"] = 0.
+    sensor716.loc[sensor716["cloud_cover"].isna(), "cloud_cover"] = 0.
+    sensor716.loc[sensor716["precip_intensity"].isna(), "precip_intensity"] = 0.
+    sensor716.loc[sensor716["precip_probability"].isna(), "precip_probability"] = 0.
+    sensor716 = sensor716.dropna()
+    return sensor716
 
-    dataframe = pp.DataFrame(data.values, datatime=datatime.values, var_names=var_names)
+def var_names():
+    return ["dayOfYear", "minuteOfDay", "dayOfWeek", "isWeekend", "temperature",
+            "humidity_sensor", "p1", "p2", "apparent_temperature", "cloud_cover", "dew_point", "humidity_weather", "precip_intensity", "precip_probability",
+            "visibility", "wind_bearing", "wind_gust", "wind_speed"]
+
+def create_tigramite_dataframe(dataset):
+    data = dataset.loc[:, var_names()]
+    datatime = dataset.loc[:, "timestamp"]
+
+    dataframe = pp.DataFrame(data.values, datatime = datatime.values, var_names=var_names())
     return dataframe
 
-
-dataframe = create_dataframe(var_names)
-# parcorr = ParCorr(significance='analytic')
-cmi_knn = CMIknn(significance='shuffle_test', knn=0.1, shuffle_neighbors=5)
+dataframe = create_tigramite_dataframe(extract_dataset(load_data()))
+parcorr = ParCorr(significance='analytic')
+# cmi_knn = CMIknn(significance='shuffle_test', knn=0.1, shuffle_neighbors=5)
 pcmci = PCMCI(
     dataframe=dataframe,
-    cond_ind_test=cmi_knn,
-    verbosity=2)
+    cond_ind_test=parcorr,
+    verbosity=1)
 
-# correlations = pcmci.get_lagged_dependencies(tau_max=20)
+
+# gpdc = GPDC(significance='analytic', gp_params=None)
+# # gpdc.generate_and_save_nulldists(sample_sizes=range(495, 501),
+# #     null_dist_filename='dc_nulldists.npz')
+# gpdc.null_dist_filename ='dc_nulldists.npz'
+# pcmci= PCMCI(
+#     dataframe=dataframe,
+#     cond_ind_test=gpdc,
+#     verbosity=1)
+
+correlations = pcmci.get_lagged_dependencies(tau_max=20)
 
 # lag_func_matrix = tp.plot_lagfuncs(val_matrix=correlations, setup_args={'var_names':var_names,
 #                                                                         'x_base':5, 'y_base':.5})
-results = pcmci.run_pcmci(tau_max=2, pc_alpha=0.05)
+import time
+start = time.time()
+results = pcmci.run_pcmci(tau_max=4, pc_alpha=0.05)
+end = time.time()
+
+print(f"Execution time: {round(end - start, 2)} seconds")
+
+import pickle
+with open('results.pickle', 'wb') as f:
+    # Pickle the 'data' dictionary using the highest protocol available.
+    pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)
 
 q_matrix = pcmci.get_corrected_pvalues(p_matrix=results['p_matrix'], fdr_method='fdr_bh')
 pcmci.print_significant_links(
