7ab221450eff97c26b1155cb68977a09744c63a8
Hendrik Makait <hendrik.makait@gmail.com>
2019-07-29 17:29:35 +0200

    Add Javadoc for FeatureTableCombination

--- air-pollution/src/main/java/de/tuberlin/dima/bdapro/jobs/FeatureTableCombination.java
+++ air-pollution/src/main/java/de/tuberlin/dima/bdapro/jobs/FeatureTableCombination.java
@@ -16,35 +16,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO: Comment EVERYTHING
+ * Flink workflow that combines the feature tables generated from sensor and weather data using a mapping feature table.
+ *
+ * @author Hendrik Makait
  */
-public class Joining extends UnifiedSensorJob {
-
+public class FeatureTableCombination extends UnifiedSensorJob {
+    // relative path of the downloaded weather data
     private static String weatherDataPath = "raw/weather/weather_data.csv";
+    // relative path of the directory containing the precalculated filtered sensor data
     private static String filterBasePath = "intermediate/";
 
+    /**
+     * Generates the combined feature table and stores it to disk.
+     */
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
-
         ParameterTool params = ParameterTool.fromArgs(args);
         final String dataDirectory = params.get("data_dir", "data");
-        FeatureTable sensor = SensorFeatureTable.generateFeatureTable(env, dataDirectory, 60, tEnv);
+
+        FeatureTable result = generateCombinedFeatureTable(env, tEnv, dataDirectory);
+
+        Path outputPath = new Path(dataDirectory, "processed/causalDiscoveryData.csv");
+        result.write(outputPath, tEnv);
+        env.execute("Generate Combined Feature Table");
+    }
+
+    /**
+     * Generates a combined feature table by combining the sensor and weather feature tables.
+     *
+     * @param env           An execution environment.
+     * @param tEnv          A table environment.
+     * @param dataDirectory The base path of the data directory.
+     * @return the combined feature table.
+     */
+    private static FeatureTable generateCombinedFeatureTable(ExecutionEnvironment env, BatchTableEnvironment tEnv, String dataDirectory) {
+        FeatureTable sensor = SensorFeatureTableGenerator.generateFeatureTable(env, dataDirectory, 60, tEnv);
         FeatureTable sensorStationMapping = generateSensorStationMappingFeatureTable(dataDirectory, env, tEnv);
         FeatureTable weather = WeatherJob.generateFeatureTable(env, tEnv);
         FeatureTable mappedSensors = sensor.join(sensorStationMapping, sensor.getKeyColumns(), "sensor_station_mapping_location = sensor_location", tEnv);
-        FeatureTable result = mappedSensors.join(weather, sensor.getKeyColumns(),
+        return mappedSensors.join(weather, sensor.getKeyColumns(),
                 "sensor_station_mapping_closest_weather_station = weather_location AND " +
-                "FLOOR(sensor_timestamp TO HOUR) = FLOOR(weather_time TO HOUR)", tEnv);
-        Path outputPath = new Path(dataDirectory, "processed/causalDiscoveryData.csv");
-        result.write(outputPath, tEnv);
-        env.execute("Joined Dataset");
+                        "FLOOR(sensor_timestamp TO HOUR) = FLOOR(weather_time TO HOUR)", tEnv);
     }
 
+    /**
+     * Creates the feature table mapping sensor locations to weather stations by loading the precalculated mapping
+     * data from disk.
+     *
+     * @param dataDirectory The base path of the data directory.
+     * @param env           An execution environment to use.
+     * @param tEnv          A table environment to use.
+     * @return the sensor station mapping feature table
+     */
     public static FeatureTable generateSensorStationMappingFeatureTable(String dataDirectory, ExecutionEnvironment env, BatchTableEnvironment tEnv) {
         DataSet<Tuple2<Double, String>> acceptedSensorData = env.readCsvFile(new Path(new Path(dataDirectory, filterBasePath), "berlin_enrichable_sensors.csv").toString())
                 .fieldDelimiter(",").ignoreFirstLine().includeFields("00001100").types(Double.class, String.class);
-        Table table = tEnv.fromDataSet(acceptedSensorData,"location," + "closest_weather_station").distinct();
+        Table table = tEnv.fromDataSet(acceptedSensorData, "location," + "closest_weather_station").distinct();
         List<Column> columns = new ArrayList<>();
         columns.add(new BasicColumn("location", TypeInformation.of(Double.class), false));
         columns.add(new BasicColumn("closest_weather_station", TypeInformation.of(String.class), false));
